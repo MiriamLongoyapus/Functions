89a18bb62d56d8f87882732630a6b2e7730200a6
MiriamLongoyapus <longoyapusmiriam@gmail.com>
2023-02-08 16:39:42 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main(){
+    greetAperson("Miriam")
+    modulus(50,13,)
+    addition(5501,1050)
+    youAreAwesome()
+
+}
+
+fun greetAperson(name: String){
+    println("hello "+name)
+}
+
+fun modulus(num1: Int, num2: Int){
+    var modulus = num1%num2
+    println(modulus)
+
+}
+fun addition(numb1: Int, num2: Int){
+    var sum = numb1+num2
+    println(sum)
+}
+
+fun youAreAwesome(){
+    println("Miriam can code")
+}
